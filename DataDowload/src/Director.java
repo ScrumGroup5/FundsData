9cf88eb79b2b6a9cdd877b5ac2094976240f48a9
e-cloud <saintscott119@gmail.com>
2014-05-25 11:10:52 +0800

    添加director

---
+++ DataDowload/src/Director.java
@@ -0,0 +1,40 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * 
+ */
+
+/**
+ * @author Saint Scott
+ *
+ */
+public class Director extends Application {
+
+	/**
+	 * 
+	 */
+	public Director() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javafx.application.Application#start(javafx.stage.Stage)
+	 */
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+}
